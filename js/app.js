7fdcb4a8c514d5a08405606dd52a092f19791785
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-27 14:15:27 +0100

    Add → added array of tasks object in Java Script Vue data

--- js/app.js
+++ js/app.js
@@ -7,11 +7,33 @@ createApp({
     data(){
         return{
             title: '(ง•̀_•́)ง',
+            todoList:[
+                {
+                    text: 'Svegliarsi',
+                    done: false,
+                },
+                {
+                    text: 'Respirare',
+                    done: false,
+                },
+                {
+                    text: 'Fare una giravolta',
+                    done: false,
+                },
+                {
+                    text: 'Respirare',
+                    done: false,
+                },
+                {
+                    text: 'Andare a dormire',
+                    done: false,
+                },
+            ],
         }
     },
 
 
-    
+
     methods:{
 
 
