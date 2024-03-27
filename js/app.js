1a47ca24d028ef78614059e92fbfdd47f47114a7
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-27 13:44:49 +0100

    First Commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+console.log('JS Test')
+
+const { createApp } = Vue;
+
+
+createApp({
+    data(){
+        return{
+            title: '(ง•̀_•́)ง',
+        }
+    },
+
+
+    
+    methods:{
+
+
+    }
+}).mount('#app')
